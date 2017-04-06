c17be81674c94dc71d38df9eba009fcd1b8625b3
Duncan <originalmyth@gmail.com>
2017-04-06 09:45:23 +0100

    first commit

---
+++ Package.swift
@@ -0,0 +1,18 @@
+import PackageDescription
+
+let package = Package(
+    name: "todolistdb",
+    dependencies: [
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
+        
+        .Package(url: "https://github.com/vapor/postgresql-provider.git", majorVersion: 1)
+    ],
+    exclude: [
+        "Config",
+        "Database",
+        "Localization",
+        "Public",
+        "Resources",
+    ]
+)
+
---
+++ Sources/App/main.swift
@@ -0,0 +1,102 @@
+import Vapor
+import VaporPostgreSQL
+import PostgreSQL
+
+
+class Task : NodeRepresentable {
+    
+    var id : Int?
+    var title : String!
+    
+    
+    func makeNode(context : Context) throws -> Node {
+        
+        return try! Node(node : ["id" : id, "title" : self.title])
+        
+    }
+    
+    init(dictionary : [String:Node]) {
+        
+        
+        guard let id = dictionary["id"]?.int, let title = dictionary["title"]?.string else {
+            
+            return
+            
+        }
+        
+        
+        self.id = id
+        self.title = title
+        
+    }
+    
+    
+}
+
+
+let drop = Droplet()
+
+try drop.addProvider(VaporPostgreSQL.Provider.self)
+
+let postgreSQL = PostgreSQL.Database(
+    dbname : "mytasks",
+    user : "admin",
+    password : "password"
+    
+)
+
+
+
+drop.get("tasks", "all") { request in
+    
+    var tasks = [Task]()
+    
+    let result = try postgreSQL.execute("SELECT * FROM tasks;")
+    
+    for dictionary in result {
+        
+        let task = Task(dictionary : dictionary)
+        tasks.append(task)
+        
+    }
+    
+    return try! JSON(node : tasks)
+    
+}
+
+drop.post("tasks") { request in
+    
+    let connection = try postgreSQL.makeConnection()
+    
+    guard let title = request.json?["title"]?.string else {
+        
+        fatalError("Title is missing")
+        
+    }
+    
+    
+    //insto table (tasks)  column (title) the first value from the next array
+    let result = try postgreSQL.execute("INSERT INTO tasks(title) VALUES($1) RETURNING id;", [title.makeNode()], on: connection)
+    
+    return "Hello world"
+    
+}
+
+
+drop.get("version") { request in
+    
+    let versionArray = try postgreSQL.execute("SELECT version()")
+    
+    guard let dictionary = versionArray.first else {
+        
+        fatalError("version not found")
+    }
+    
+    let version = dictionary["version"]
+    
+    return try! JSON(node : version)
+    
+    
+}
+
+drop.run()
